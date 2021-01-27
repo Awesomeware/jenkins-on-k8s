82e658d7f88f4bd71916b95a17d6f5a715c5ad05
Martin Grunbaum <martin.grunbaum@pleo.io>
2021-01-27 17:49:38 +0100

    Remove groovy config not needed

--- examples/github-oauth/configuration/groovy/set-authorization-strategy.groovy
+++
@@ -1,8 +0,0 @@
-import jenkins.model.Jenkins
-import hudson.security.FullControlOnceLoggedInAuthorizationStrategy
-
-// Lets make it so if you aren't logged in, you can see jobs, but cannot go to 'Manage Jenkins'
-def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
-strategy.setAllowAnonymousRead(true)
-Jenkins.instance.setAuthorizationStrategy(strategy)
-Jenkins.instance.save()
